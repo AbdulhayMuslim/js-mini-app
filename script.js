6a663c983edb70b8ec7a9aef4779dc15c5763772
Abdulhay Muslim <AbdulhayMuslim@users.noreply.github.com>
2026-02-26 16:46:34 +0430

    products data fetched from API

---
+++ script.js
@@ -0,0 +1,20 @@
+async function fetchData() {
+    try {
+      const response = await fetch('https://fakestoreapi.com/products');
+      if (!response.ok) {
+        throw new Error('Network Response Failed! ' + response.status);
+      }
+      const data = await response.json();
+      console.log(data);
+    }
+    
+    catch (error) {
+      console.error('Fetch Error: ', error);
+    }
+  }
+  
+  const fetchProduct = fetchData();
+
+const productsBox = document.querySelector(".products");
+
+productsBox.appendChild(fetchProduct);
